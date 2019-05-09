d42162fb17af50c1fc7c1908f22b918668ddacba
Dionicio Acevedo <tec.dionicioacevedo@gmail.com>
2019-05-09 17:42:26 -0400

    Prueba con la base de datos, insert y queries

---
+++ db/sqlite_structure.sql
@@ -0,0 +1,19 @@
+CREATE TABLE Notes
+(
+    _id             INTEGER  PRIMARY KEY,
+    _note_title     TEXT NOT NULL DEFAULT '',
+    _note_message   TEXT NOT NULL DEFAULT ''
+);
+
+CREATE TABLE Tags
+(
+    _tag_name TEXT UNIQUE NOT NULL
+);
+
+CREATE TABLE NoteTags
+(
+    _note_id INTEGER  NOT NULL,
+    _tag     TEXT NOT NULL,
+    FOREIGN KEY (_note_id) REFERENCES Notes (_id) ON DELETE CASCADE,
+    FOREIGN KEY (_tag)     REFERENCES Tags  (_tag_name)
+)
