7b3084f858aa6d84a48e4cda8b86fcba2342747a
Dionicio Acevedo <tec.dionicioacevedo@gmail.com>
2019-05-10 08:42:51 -0400

    Ahora se puede enviar el usuario por la URL

--- index.php
+++ index.php
@@ -5,7 +5,13 @@ require 'vendor/autoload.php';
 use App\SQLiteConnection;
 use App\Config;
 
-$username = 'elPapuXd';
+
+if(!isset($_REQUEST['user'])) {
+    echo 'Salida prematura';
+    die();
+}
+$username = $_REQUEST['user'];
+
 $userdb = Config::build_sqlitedb_name([$username]);
 
 //echo Config::build_sqlitedb_name([$username]);
@@ -15,17 +21,17 @@ $pdo = (new SQLiteConnection()) -> get_instance($userdb, Config::PATH_SCHEMA_DAT
 
 if($pdo != null)
 {
+  /*  echo '<h4>Estamos conectados con sqlite</h4>';
     $tags = ['dionicio', 'nuevo', 'noticias'];
-    echo '<h4>Estamos conectados con sqlite</h4>';
 
-    /*$statement = $pdo -> prepare("INSERT INTO Notes(_note_title, _note_message) VALUES (:title, :msg)");
-    $statement -> bindValue(':title', "Titulo 1");
+    $statement = $pdo -> prepare("INSERT INTO Notes(_note_title, _note_message) VALUES (:title, :msg)");
+    $statement -> bindValue(':title', "Titulo 1(".$username.")");
     $statement -> bindValue(':msg', "Hey Hey!");
     $statement -> execute();
     echo $pdo -> lastInsertId();
     echo $statement -> rowCount();
-    */
-/*
+    
+
     foreach ($tags as $key => $value) {        
         $statement = $pdo -> prepare("INSERT INTO Tags(_tag_name) VALUES (?)");
         //$statement -> bindParam($value);
@@ -33,30 +39,31 @@ if($pdo != null)
             [$value]
         );
         
-        echo $value."*".$statement -> rowCount()."*";
+       // echo $value."*".$statement -> rowCount()."*";
 
         if($statement -> rowCount() < 1){
             break;
         }
-    }*/
-    /*foreach ($tags as $key => $value) {        
+    }
+    foreach ($tags as $key => $value) {        
         $statement = $pdo -> prepare("INSERT INTO NoteTags(_note_id, _tag) VALUES (1,?)");
         $statement -> execute(
             [$value]
         );
         
-        echo $value."*".$statement -> rowCount()."*";
+        //echo $value."*".$statement -> rowCount()."*";
 
         if($statement -> rowCount() < 1){
             break;
         }
-    }*/
+    }
+   // */
     $statement = $pdo -> prepare("SELECT * FROM Notes WHERE _id = 1");
     $statement -> execute();
 
 
     $result = $statement -> fetch(PDO::FETCH_ASSOC);
-    echo "<h1>Titulo de la nota".$result['_note_title']."</h1><br>";
+    echo "<h1>".$result['_note_title']."</h1><br>";
     
 
     $statement = $pdo -> prepare("SELECT * FROM NoteTags WHERE _note_id = 1");
@@ -66,7 +73,7 @@ if($pdo != null)
     $results = $statement -> fetchAll(PDO::FETCH_ASSOC);
     
     foreach ($results as $key => $value) {
-        echo $value['_tag'].",";
+        echo "<strong>".$value['_tag']."</strong>,";
     }
 
 
